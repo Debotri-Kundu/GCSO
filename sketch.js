4689ba854d80772427baf6fa1178b6c5dca1a516
Debotri-Kundu <73526091+Debotri-Kundu@users.noreply.github.com>
2020-12-18 00:08:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var speed,weight;
-var car,wall;
+var bullet,wall,thickness;
 
 
 
@@ -7,34 +7,31 @@ function setup() {
   createCanvas(1200,400);
   speed=random(55,90);
   weight=random(400,1500);
+  thickness=random(22,83)
 
+  bullet=createSprite(50, 200, 70, 20);
+  bullet.velocityX=speed;
+  bullet.shapeColor=color(255)
 
-  car=createSprite(50, 200, 50, 50);
-  car.velocityX=speed;
-  car.shapeColor=color(255)
-
-  wall=createSprite(1150, 200, 60, height/2);
+  wall=createSprite(1150, 200, thickness, height/2);
   wall.shapeColor=color(80,80,80)
 }
 
 function draw() {
   background("black");
   
-  if(wall.x-car.x<(car.width+wall.width)/2)
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2)
   {
-    car.velocityX=0;
-    var deformation=0.5 * weight * speed * speed/22500
-    if(deformation>180)
+    bullet.velocityX=0;
+    var damage=(0.5 * weight * speed * speed)/(thickness*thickness*thickness)
+    if(damage>10)
     {
-      car.shapeColor=color(255,0,0)
+      wall.shapeColor=color(255,0,0)
     }
-    if(deformation<180 &&  deformation>80)
+    
+    if(damage<10)
     {
-      car.shapeColor=color(230,230,0)
-    }
-    if(deformation<80)
-    {
-      car.shapeColor=color(0,255,0)
+      wall.shapeColor=color(0,255,0)
     }
   }
   drawSprites();
